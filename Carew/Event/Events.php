5745edd84fa2a7978602c855d52f1e71d707488b
Grégoire Pineau <lyrixx@lyrixx.info>
2013-02-05 21:41:39 +0100

    Added some phpdoc

--- Carew/Event/Events.php
+++ Carew/Event/Events.php
@@ -4,13 +4,48 @@ namespace Carew\Event;
 
 final class Events
 {
-    const DOCUMENT  = 'carew.document';
-    const DOCUMENTS = 'carew.documents';
-    const POST      = 'carew.post';
-    const PAGE      = 'carew.page';
+    /**
+     * Dispatch for each document during the document creation
+     */
+    const DOCUMENT = 'carew.document';
+
+    /**
+     * Dispatch for each post during the document creation
+     */
+    const POST = 'carew.post';
+
+    /**
+     * Dispatch for each page during the document creation
+     */
+    const PAGE = 'carew.page';
+
+    /**
+     * Dispatch for each api during the document creation
+     */
     const API       = 'carew.api';
-    const INDEX     = 'carew.index';
-    const INDEXES   = 'carew.indexes';
-    const TAG       = 'carew.tag';
-    const TAGS      = 'carew.tags';
+
+    /**
+     * Dispatch with all documents after creation
+     */
+    const DOCUMENTS = 'carew.documents';
+
+    /**
+     * Dispatch for each index during the document creation
+     */
+    const INDEX = 'carew.index';
+
+    /**
+     * Dispatch with all indexes after creation
+     */
+    const INDEXES = 'carew.indexes';
+
+    /**
+     * Dispatch for each index during the document creation
+     */
+    const TAG = 'carew.tag';
+
+    /**
+     * Dispatch with all tags after creation
+     */
+    const TAGS = 'carew.tags';
 }
